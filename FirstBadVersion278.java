39a29d23d9701915e520039e1404e6bf7da80690
dawndawnwong <dawndawnwong@gmail.com>
2021-01-22 00:24:31 -0800

    Update FirstBadVersion278.java

--- FirstBadVersion278.java
+++ FirstBadVersion278.java
@@ -1,6 +1,6 @@
 public class FirstBadVersion278 {
     public int firstBadVersion(int n) {
-        int start = 0, end = n;
+        int start = 1, end = n;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (isBadVersion(mid)) {
