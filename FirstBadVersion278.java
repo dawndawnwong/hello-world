0f0c4b50fcfa10cfaceae6d67979932494b3e99b
dawndawnwong <dawndawnwong@gmail.com>
2021-04-27 00:16:20 -0700

    278FirstBadVersion Update

--- FirstBadVersion278.java
+++ FirstBadVersion278.java
@@ -1,5 +1,6 @@
 public class FirstBadVersion278 {
     public int firstBadVersion(int n) {
+        //Time complexity : O(Logn), space complexity: O(1)
         int start = 1, end = n;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
