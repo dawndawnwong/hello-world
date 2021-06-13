905e0dffc897f2c99bed30891088a9ce307d829b
dawndawnwong <65267688+dawndawnwong@users.noreply.github.com>
2021-06-12 21:33:42 -0700

    Update 349. Intersection of Two Arrays

--- IntersectionofTwoArrays349.java
+++ IntersectionofTwoArrays349.java
@@ -1,4 +1,5 @@
 public class IntersectionofTwoArrays349 {
+    // Binary search, time complexity: O(nlogn)
     public int[] intersection(int[] nums1, int[] nums2) {
         if (nums1 == null || nums1.length == 0)
             return new int[] {};
@@ -11,6 +12,7 @@ public class IntersectionofTwoArrays349 {
                 set.add(nums2[i]);
             }
         }
+
         int[] result = new int[set.size()];
         int k = 0;
         for (int num : set) {
@@ -37,5 +39,52 @@ public class IntersectionofTwoArrays349 {
         } else {
             return false;
         }
+    }
+
+    // Sort both arrays using two pointers, time complexity: O(nlogn)
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (int num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+
+    // Two hash sets, time complexity: O(n)
+    public int[] intersection3(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set1.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+
+        int[] result = new int[intersect.size()];
+        int k = 0;
+        for (int num : intersect) {
+            result[k++] = num;
+        }
+        return result;
     }
 }
