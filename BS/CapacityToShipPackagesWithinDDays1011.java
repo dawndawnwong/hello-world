1b252c06b955f64318fd403b6647532a41bd524c
dawndawnwong <65267688+dawndawnwong@users.noreply.github.com>
2021-06-12 23:36:15 -0700

    1101. Capacity To Ship Packages Within D Days

--- BS/CapacityToShipPackagesWithinDDays1011.java
+++ BS/CapacityToShipPackagesWithinDDays1011.java
@@ -1,36 +1,38 @@
 public class CapacityToShipPackagesWithinDDays1011 {
-    public int shipWithinDays(int[] weights, int D) {
+    public int shipWithinDays(int[] weights, int days) {
         int start = 0, end = 0;
         for (int w : weights) {
             start = Math.max(start, w);
             end += w;
         }
+
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            boolean canShip = canShip(weights, D, mid);
+            boolean canShip = canShip(weights, days, mid);
             if (canShip) {
                 end = mid;
             } else {
                 start = mid;
             }
         }
-        if (canShip(weights, D, start)) {
+
+        if (canShip(weights, days, start)) {
             return start;
         } else {
             return end;
         }
     }
 
-    public boolean canShip(int[] weights, int D, int capacity) {
-        int needsDay = 1;
+    private boolean canShip(int[] weights, int days, int capacity) {
+        int needDay = 1;
         int curWeights = 0;
         for (int w : weights) {
             if (curWeights + w > capacity) {
-                needsDay++;
+                needDay++;
                 curWeights = 0;
             }
             curWeights += w;
         }
-        return (needsDay <= D);
+        return (needDay <= days);
     }
 }
