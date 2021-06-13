7227520e8172fd1acdf722b52ca002146c491228
dawndawnwong <65267688+dawndawnwong@users.noreply.github.com>
2021-06-12 23:56:31 -0700

    1101. Capacity To Ship Packages Within D Days

--- BS/CapacityToShipPackagesWithinDDays1011.java
+++ BS/CapacityToShipPackagesWithinDDays1011.java
@@ -35,4 +35,28 @@ public class CapacityToShipPackagesWithinDDays1011 {
         }
         return (needDay <= days);
     }
+
+    // Binary Search with one method
+    public int shipWithinDays2(int[] weights, int days) {
+        int start = 0, end = 0;
+        for (int w : weights) {
+            start = Math.max(start, w);
+            end += w;
+        }
+        while (start < end) {
+            int mid = start + (end - start) / 2, needDays = 1, curWeight = 0;
+            for (int w : weights) {
+                if (curWeight + w > mid) {
+                    needDays++;
+                    curWeight = 0;
+                }
+                curWeight += w;
+            }
+            if (needDays > days)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return start;
+    }
 }
